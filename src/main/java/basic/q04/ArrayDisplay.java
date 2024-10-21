7780c802263dfe27187956a70d1619262d6fb666
s-Mihara <s-mihara@seassist.co.jp>
2024-10-21 15:40:21 +0900

    add 1-Java01

---
+++ src/main/java/basic/q04/ArrayDisplay.java
@@ -0,0 +1,11 @@
+package basic.q04;
+
+public class ArrayDisplay {
+
+	public static void main(String[] args) {
+		String[] fruits = {"りんご", "ばなな", "みかん", "ぶどう", "すいか"};
+		for (String fluit : fruits) {
+			System.out.println(fluit);
+		}
+	}
+}
